aa25b9dc8974e430284f6bad05436005362ebfcc
Bruno Henrique 'FluffyBucketSnake' Glowaski Morais <fluffybucketsnake@outlook.com>
2024-03-03 17:17:30 -0400

    feat: line rendering

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,17 @@
+mod color;
+mod framebuffer;
+
+use color::{Color, WHITE};
+use framebuffer::Framebuffer;
 use minifb::{Key, KeyRepeat, Window, WindowOptions};
+use nalgebra_glm::{vec2, Vec2};
 
 const WINDOW_TITLE: &str = "Rasterization in One Weekend";
 const WINDOW_WIDTH: usize = 640;
 const WINDOW_HEIGHT: usize = 360;
 
 fn main() {
-    let mut buffer: Vec<u32> = vec![0; WINDOW_WIDTH * WINDOW_HEIGHT];
-
+    let mut framebuffer = Framebuffer::new(WINDOW_WIDTH, WINDOW_HEIGHT);
     let mut window = Window::new(
         WINDOW_TITLE,
         WINDOW_WIDTH,
@@ -14,25 +19,28 @@ fn main() {
         WindowOptions::default(),
     )
     .unwrap();
-
     window.limit_update_rate(Some(std::time::Duration::from_micros(1000 / 60)));
 
+    let vertices = [vec2(320.0, 90.0), vec2(160.0, 270.0), vec2(480.0, 270.0)];
     while window.is_open() && !window.is_key_pressed(Key::Escape, KeyRepeat::No) {
-        for y in 0..WINDOW_HEIGHT {
-            for x in 0..WINDOW_WIDTH {
-                let i = y * WINDOW_WIDTH + x;
-                let on_left_half = x / (WINDOW_WIDTH / 2) == 0;
-                let on_upper_half = y / (WINDOW_HEIGHT / 2) == 0;
-                buffer[i] = if on_left_half ^ on_upper_half {
-                    0x00FFFFFF
-                } else {
-                    0x00000000
-                };
-            }
-        }
+        draw_line(&mut framebuffer, vertices[0], vertices[1], WHITE);
+        draw_line(&mut framebuffer, vertices[1], vertices[2], WHITE);
+        draw_line(&mut framebuffer, vertices[2], vertices[0], WHITE);
 
-        window
-            .update_with_buffer(&buffer, WINDOW_WIDTH, WINDOW_HEIGHT)
-            .unwrap();
+        framebuffer.update_window(&mut window);
+    }
+}
+
+fn draw_line(framebuffer: &mut Framebuffer, start: Vec2, end: Vec2, color: Color) {
+    let diff = end - start;
+    let step = diff.abs().max();
+    let derivative = diff / step;
+    let step = step as usize;
+    let mut current = start;
+    let mut i = 0;
+    while i <= step {
+        framebuffer.set_color((current.x as usize, current.y as usize), color);
+        current += derivative;
+        i += 1;
     }
 }

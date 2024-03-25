d1557f77d31c95bc6e34aa769c63d32002f61af1
Bruno Henrique 'FluffyBucketSnake' Glowaski Morais <fluffybucketsnake@outlook.com>
2024-03-25 01:05:02 -0400

    fix: better raster precision with fixed point arithmetic

--- src/rasterization.rs
+++ src/rasterization.rs
@@ -1,48 +1,67 @@
-use nalgebra_glm::{vec2, vec3, Vec2, Vec3};
+use nalgebra_glm::{ceil, floor, vec2, vec3, RealNumber, TVec2, Vec2, Vec3};
+use simba::scalar::FixedI28F4;
+
+type FVec2 = TVec2<FixedI28F4>;
+
+const EPSILON: FixedI28F4 = FixedI28F4::from_bits(0x01);
 
 pub fn rasterize_solid_triangle(vertices: &[Vec2; 3], mut f: impl FnMut(Vec2, Vec3)) {
-    let [c0, c1, c2] = *vertices;
+    let [c0, c1, c2] = vertices.clone().map(vec2_to_fvec2);
 
-    let min = c0.inf(&c1.inf(&c2));
-    let max = c0.sup(&c1.sup(&c2));
+    let min = floor(&c0.inf(&c1.inf(&c2)));
+    let max = ceil(&c0.sup(&c1.sup(&c2)));
 
-    let signed_area = edge_function(c0, c1, c2);
-    let u_bias = left_or_top_edge_bias(c1, c2);
-    let v_bias = left_or_top_edge_bias(c2, c0);
-    let w_bias = left_or_top_edge_bias(c0, c1);
+    let signed_area = edge_function(c0, c1, c2).0.to_num::<f32>();
+
+    if signed_area < 0.0 {
+        return;
+    }
+
+    let u_bias = left_or_top_edge_bias(c1, c2, EPSILON);
+    let v_bias = left_or_top_edge_bias(c2, c0, EPSILON);
+    let w_bias = left_or_top_edge_bias(c0, c1, EPSILON);
+    let half = FixedI28F4::from_bits(0b1000);
 
     let mut y = min.y;
     while y <= max.y {
         let mut x = min.x;
         while x <= max.x {
-            let p = vec2(x, y);
+            let p = vec2(x + half, y + half);
             let u = edge_function(c1, c2, p) + u_bias;
             let v = edge_function(c2, c0, p) + v_bias;
             let w = edge_function(c0, c1, p) + w_bias;
 
-            if u >= 0.0 && v >= 0.0 && w >= 0.0 {
-                f(vec2(x.trunc(), y.trunc()), vec3(u, v, w) / signed_area)
+            if u >= num::zero() && v >= num::zero() && w >= num::zero() {
+                f(
+                    vec2(x, y).map(|c| c.0.to_num()),
+                    vec3(u, v, w).map(|c| c.0.to_num::<f32>()) / signed_area,
+                )
             }
-            x += 1.0;
+            x += num::one();
         }
-        y += 1.0;
+        y += num::one();
     }
 }
 
 #[inline]
-fn edge_function(v0: Vec2, v1: Vec2, v2: Vec2) -> f32 {
-    Vec2::perp(&(v2 - v0), &(v1 - v0))
+fn vec2_to_fvec2(src: Vec2) -> FVec2 {
+    src.map(|c| FixedI28F4::from_num(c))
 }
 
 #[inline]
-fn left_or_top_edge_bias(start: Vec2, end: Vec2) -> f32 {
+fn edge_function<T: RealNumber>(v0: TVec2<T>, v1: TVec2<T>, v2: TVec2<T>) -> T {
+    TVec2::perp(&(v2 - v0), &(v1 - v0))
+}
+
+#[inline]
+fn left_or_top_edge_bias<T: RealNumber>(start: TVec2<T>, end: TVec2<T>, epsilon: T) -> T {
     let edge = end - start;
-    let is_left_edge = edge.y > 0.0;
-    let is_top_edge = edge.y == 0.0 && edge.x < 0.0;
+    let is_left_edge = edge.y > num::zero();
+    let is_top_edge = edge.y == num::zero() && edge.x < num::zero();
     if is_left_edge || is_top_edge {
-        return 0.0;
+        return num::zero();
     } else {
-        return -1.0 / 16.0;
+        return -epsilon;
     }
 }
 
